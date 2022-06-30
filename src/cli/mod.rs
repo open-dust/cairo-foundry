54933450ab30640e661ea684e3868685da6b1907
Anthony Buisset <abuisset@gmail.com>
2022-06-30 14:28:26 +0200

    feat: list command can find test files recursively

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,11 +1,17 @@
 use clap::Parser;
 
+/**
+ * Commands module
+ *
+ * This module contains all available commands of the CLI
+ */
 pub mod commands;
 
 use commands::Commands;
 
+/// Store the command line arguments
 #[derive(Parser)]
-#[clap(author, version, about, long_about = None)]
+#[clap(version, about)]
 #[clap(propagate_version = true)]
 pub struct Args {
 	/// command to run
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
+//! Run efficiently your unit tests written in cairo
+
+/**
+ * CLI Parsing module
+ *
+ * Parses the argument list and execute the given sub-command
+ */
 pub mod cli;
