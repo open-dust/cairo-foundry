a52d17140fad8c872c69f8b44797fd61687ee00e
Stanislas Bernard <bernardstanislas@gmail.com>
2022-10-28 22:47:58 +0100

    :recycle: Isolate single entrypoint test runner function

--- src/cli/commands/test/tests.rs
+++ src/cli/commands/test/tests.rs
@@ -1,6 +1,17 @@
 use crate::cli::commands::{test::TestArgs, CommandExecution};
 use std::path::PathBuf;
 
+use super::{compile_and_list_entrypoints, setup_hint_processor, test_single_entrypoint};
+
+pub fn run_single_test(test_name: &str, test_path: &PathBuf) -> (String, bool) {
+	let (_, path_to_compiled, _) = compile_and_list_entrypoints(test_path.to_owned()).unwrap();
+	test_single_entrypoint(
+		&path_to_compiled,
+		test_name.to_string(),
+		&setup_hint_processor(),
+	)
+}
+
 #[test]
 fn test_cairo_contracts() {
 	TestArgs {
