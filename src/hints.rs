85f165d626154766545b9d418d0870a4bdfc569a
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-10-28 12:39:40 +0200

    feat: simplify static hint buffer

--- src/hints.rs
+++ src/hints.rs
@@ -9,40 +9,32 @@ use cairo_rs::{
 };
 use lazy_static::lazy_static;
 use num_traits::cast::ToPrimitive;
-use std::{
-	collections::HashMap,
-	io::Write,
-	sync::{Arc, Mutex, RwLock},
-};
+use std::{collections::HashMap, io::Write, sync::RwLock};
 use uuid::Uuid;
 
 pub const EXECUTION_UUID_VAR_NAME: &str = "cairo-foundry-execution-uuid";
 
 lazy_static! {
-	static ref HINT_OUTPUT_BUFFER: RwLock<HashMap<Uuid, Arc<Mutex<Vec<u8>>>>> =
-		RwLock::new(HashMap::new());
+	static ref HINT_OUTPUT_BUFFER: RwLock<HashMap<Uuid, Vec<u8>>> = RwLock::new(HashMap::new());
 }
 
 pub fn init_buffer(execution_uuid: Uuid) {
-	HINT_OUTPUT_BUFFER
-		.write()
-		.unwrap()
-		.insert(execution_uuid, Arc::new(Mutex::new(Vec::new())));
+	HINT_OUTPUT_BUFFER.write().unwrap().insert(execution_uuid, Vec::new());
 }
 
 pub fn clear_buffer(execution_uuid: &Uuid) {
 	HINT_OUTPUT_BUFFER.write().unwrap().remove(execution_uuid);
 }
 
-pub fn get_buffer(execution_uuid: &Uuid) -> Option<Arc<Mutex<Vec<u8>>>> {
+pub fn get_buffer(execution_uuid: &Uuid) -> Option<Vec<u8>> {
 	HINT_OUTPUT_BUFFER.read().unwrap().get(execution_uuid).cloned()
 }
 
 fn write_to_output_buffer(execution_uuid: &Uuid, data: &[u8]) {
-	let hashmap_lock = HINT_OUTPUT_BUFFER.read().unwrap();
-	let opt_buffer = hashmap_lock.get(execution_uuid);
+	let mut hashmap_lock = HINT_OUTPUT_BUFFER.write().unwrap();
+	let opt_buffer = hashmap_lock.get_mut(execution_uuid);
 	if let Some(buffer) = opt_buffer {
-		buffer.lock().unwrap().write_all(data).unwrap();
+		buffer.write_all(data).unwrap();
 	}
 }
 
