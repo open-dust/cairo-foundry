7edd8c784a3fcebf1a65bbbc8717836f994746b5
internos <amajidsinar@gmail.com>
2022-12-07 01:22:21 +0700

    feat: fix test

--- src/cli/commands/test/tests.rs
+++ src/cli/commands/test/tests.rs
@@ -1,10 +1,18 @@
 use crate::cli::commands::{test::TestArgs, CommandExecution};
 use std::path::PathBuf;
 
-use super::{compile_and_list_entrypoints, setup_hint_processor, test_single_entrypoint};
+use super::{compile_and_list_entrypoints, setup_hint_processor, test_single_entrypoint, CompiledCacheFile, CacheStatus};
+
+
+
 
 pub fn run_single_test(test_name: &str, test_path: &PathBuf) -> (String, bool) {
-	let (_, path_to_compiled, _) = compile_and_list_entrypoints(test_path.to_owned()).unwrap();
+	let cache_file = CompiledCacheFile {
+		path: test_path.to_owned(),
+		status: CacheStatus::Cached,
+	};
+	
+	let (_, path_to_compiled, _) = compile_and_list_entrypoints(Ok(cache_file)).unwrap();
 	test_single_entrypoint(
 		&path_to_compiled,
 		test_name.to_string(),
