a43eb506328d221943341de872eb1a34bd608ca6
Bachir ARIF <arif.bachir@hotmail.com>
2023-01-26 16:33:02 +0100

    Feat: Build Processor Regex added first test

--- src/hints/hint_processor/function_like_hint_processor/tests.rs
+++ src/hints/hint_processor/function_like_hint_processor/tests.rs
@@ -2,8 +2,14 @@ use crate::cli::commands::test::{tests::run_single_test, TestCommandError, TestS
 use rstest::rstest;
 
 #[rstest]
-#[case("src/hints/hint_processor/function_like_hint_processor/test_cairo_programs/wrong_custom_hint.cairo", TestStatus::FAILURE)]
-fn wrong_custom_hint(#[case] path: &str, #[case] expected_success: TestStatus) -> Result<(), TestCommandError> {
+#[case(
+	"src/hints/hint_processor/function_like_hint_processor/test_cairo_programs/wrong_custom_hint.cairo",
+	TestStatus::FAILURE
+)]
+fn wrong_custom_hint(
+	#[case] path: &str,
+	#[case] expected_success: TestStatus,
+) -> Result<(), TestCommandError> {
 	let path = std::path::PathBuf::from(path);
 	let result = run_single_test("test_wrong_custom_hint", &path).expect("Should be Ok").success;
 	assert_eq!(expected_success, result);
