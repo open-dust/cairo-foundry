9d74162dd2471937c92a978888e0a678311976d2
Haroune Mohammedi <haroune.mohammedi@quadratic-labs.com>
2023-02-06 11:08:33 +0100

    refactor(cache): use cache_dir utility instead of dirs::cache_dir

--- src/compile/cache/mod.rs
+++ src/compile/cache/mod.rs
@@ -14,25 +14,26 @@ pub struct Cache {
 	pub hash: String,
 }
 
-#[derive(Error, Debug)]
-pub enum CacheError {
-	#[error(transparent)]
-	FileNotFoundError(#[from] io::Error),
-	#[error(transparent)]
-	DeserializeError(#[from] serde_json::Error),
-	#[error("cache directory does not exist on this platform")]
-	CacheDirNotSupportedError,
-	#[error("filename does not exist")]
-	InvalidContractExtension(PathBuf),
-	#[error(transparent)]
-	StripPrefixError(#[from] std::path::StripPrefixError),
-}
 // CacheDirNotSupported is a top level struct and not an enum variant because
 // it's converted elsewhere to other errors using [#from] and we want to be
 // as specific as possible
 #[derive(Error, Debug)]
 #[error("cache directory does not exist on this platform")]
 pub struct CacheDirNotSupported;
+
+#[derive(Error, Debug)]
+pub enum CacheError {
+	#[error(transparent)]
+	FileNotFoundError(#[from] io::Error),
+	#[error(transparent)]
+	DeserializeError(#[from] serde_json::Error),
+	#[error(transparent)]
+	CacheDirNotSupportedError(#[from] CacheDirNotSupported),
+	#[error("filename does not exist")]
+	InvalidContractExtension(PathBuf),
+	#[error(transparent)]
+	StripPrefixError(#[from] std::path::StripPrefixError),
+}
 
 pub const CAIRO_FOUNDRY_CACHE_DIR: &str = "cairo-foundry-cache";
 pub const CAIRO_FOUNDRY_COMPILED_CONTRACT_DIR: &str = "compiled-cairo-files";
@@ -68,11 +69,10 @@ fn is_valid_cairo_contract(contract_path: &PathBuf) -> Result<(), CacheError> {
 fn get_cache_path(contract_path: &PathBuf, root_dir: &PathBuf) -> Result<PathBuf, CacheError> {
 	// check if contract_path have .cairo extension
 	is_valid_cairo_contract(contract_path)?;
-	let cache_dir = dirs::cache_dir().ok_or(CacheError::CacheDirNotSupportedError)?;
 	// get relative dir path from root_dir
 	let contract_relative_path = contract_path.strip_prefix(root_dir)?;
 
-	let mut cache_path = cache_dir.join(CAIRO_FOUNDRY_CACHE_DIR).join(contract_relative_path);
+	let mut cache_path = cache_dir()?.join(CAIRO_FOUNDRY_CACHE_DIR).join(contract_relative_path);
 	cache_path.set_extension("json");
 	Ok(cache_path)
 }
@@ -83,10 +83,10 @@ fn get_compiled_contract_path(
 ) -> Result<PathBuf, CacheError> {
 	// check if contract_path have .cairo extension
 	is_valid_cairo_contract(contract_path)?;
-	let cache_dir = dirs::cache_dir().ok_or(CacheError::CacheDirNotSupportedError)?;
 	let contract_relative_path = contract_path.strip_prefix(root_dir)?;
-	let mut compiled_contract_path =
-		cache_dir.join(CAIRO_FOUNDRY_COMPILED_CONTRACT_DIR).join(contract_relative_path);
+	let mut compiled_contract_path = cache_dir()?
+		.join(CAIRO_FOUNDRY_COMPILED_CONTRACT_DIR)
+		.join(contract_relative_path);
 	compiled_contract_path.set_extension("json");
 	Ok(compiled_contract_path)
 }
