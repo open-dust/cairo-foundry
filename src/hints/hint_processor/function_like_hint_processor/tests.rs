39a4972b5f355328bfdda2cb2eb9e5a2c44931b2
Bachir ARIF <arif.bachir@hotmail.com>
2023-02-08 15:18:49 +0100

    TEST: Adding test about the list of arguments

--- src/hints/hint_processor/function_like_hint_processor/tests.rs
+++ src/hints/hint_processor/function_like_hint_processor/tests.rs
@@ -8,7 +8,9 @@ use std::{collections::HashMap, rc::Rc};
 
 use crate::{
 	hints,
-	hints::hint_processor::function_like_hint_processor::{FunctionLikeHintProcessor, HintFunc},
+	hints::hint_processor::function_like_hint_processor::{
+		Code, FunctionLikeHintProcessor, FunctionLikeHintProcessorData, HintFunc,
+	},
 };
 use rstest::rstest;
 
@@ -38,5 +40,70 @@ fn test_function_like_hint_should_return_unknown_hint_error(
 	);
 
 	assert_matches!(result, Err(VirtualMachineError::UnknownHint(code)) if code == *hint_code.to_string());
+	Ok(())
+}
+
+#[rstest]
+#[case("expect_revert")]
+fn test_function_like_hint_should_return_rawcode_hint(
+	#[case] hint_code: &str,
+) -> Result<(), VirtualMachineError> {
+	let mut hint_processor = FunctionLikeHintProcessor::default();
+	let expect_revert_hint = Rc::new(HintFunc(Box::new(hints::expect_revert)));
+	hint_processor.add_hint(String::from("expect_revert"), expect_revert_hint);
+
+	let hint_data = hint_processor
+		.compile_hint(
+			hint_code,
+			&Default::default(),
+			&Default::default(),
+			&Default::default(),
+		)
+		.unwrap();
+
+	let hint_data_return = hint_data
+		.downcast_ref::<FunctionLikeHintProcessorData>()
+		.ok_or(VirtualMachineError::WrongHintData)?;
+
+	assert_eq!(
+		Code::RawCode(String::from("expect_revert")),
+		hint_data_return.code
+	);
+	Ok(())
+}
+
+#[rstest]
+#[case("expect_revert(arg0, arg1, arg2)")]
+fn test_function_like_hint_should_return_function_name_and_args_hint(
+	#[case] hint_code: &str,
+) -> Result<(), VirtualMachineError> {
+	let mut hint_processor = FunctionLikeHintProcessor::default();
+	let expect_revert_hint = Rc::new(HintFunc(Box::new(hints::expect_revert)));
+	hint_processor.add_hint(String::from("expect_revert"), expect_revert_hint);
+
+	let hint_data = hint_processor
+		.compile_hint(
+			hint_code,
+			&Default::default(),
+			&Default::default(),
+			&Default::default(),
+		)
+		.unwrap();
+
+	let hint_data_return = hint_data
+		.downcast_ref::<FunctionLikeHintProcessorData>()
+		.ok_or(VirtualMachineError::WrongHintData)?;
+
+	assert_eq!(
+		Code::Function(
+			String::from("expect_revert"),
+			Vec::from([
+				String::from("arg0"),
+				String::from("arg1"),
+				String::from("arg2")
+			])
+		),
+		hint_data_return.code
+	);
 	Ok(())
 }
